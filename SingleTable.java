c11f779be132066f7c6e0437ee195526026e89ea
lucyimhoff <lucyi4@nycstudents.net>
2025-05-04 21:49:43 +0000

    finished

--- SingleTable.java
+++ SingleTable.java
@@ -1,6 +1,27 @@
 public class SingleTable{
+    private int seats;
+    private double viewQuality;
+    private int height;
     
-    public SingleTable(){
+    public SingleTable(int s, double v, int h){
+        seats = s;
+        viewQuality = v;
+        height = h;
+    }
 
+    public int getNumSeats(){
+        return seats;
+    }
+    
+    public int getHeight(){
+        return height;
+    }
+
+    public double getViewQuality(){
+        return viewQuality;
+    }
+
+    public void setViewQuality(double value){
+        viewQuality = value;
     }
 }
